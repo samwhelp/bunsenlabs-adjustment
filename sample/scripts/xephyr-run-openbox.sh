92e27903de86cd3c3c726c344787a120a107a7f0
samwhelp <samwhelp@users.noreply.github.com>
2024-02-05 21:58:32 +0800

    fix

---
+++ sample/scripts/xephyr-run-openbox.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+
+Xephyr :100 -ac -screen 1280x680 &
+XEPHYR_PID=$!
+sleep 0.5
+
+DISPLAY=:100 openbox-session
+kill ${XEPHYR_PID}
+
+
+################################################################################
+###
+##
+
+
+##
+## ## Original Script
+## * https://github.com/worron/awesome-config/blob/master/scripts/run-with-xephyr.sh
+##
+
+
+##
+## ## Reference
+## * https://wiki.archlinux.org/index.php/Xephyr
+##
+
+##
+## ## Openbox xsession
+##
+## run
+##
+## ``` sh
+## grep '^Exec=' /usr/share/xsessions/openbox.desktop
+## ```
+##
+## show
+##
+## ```
+## Exec=/usr/bin/openbox-session
+## ```
+##
+
+
+##
+###
+################################################################################
